30d3ce450ee6679eb7331ef020d3e09ab3be85ad
Vivek <vivekprasad1410@gmail.com>
2020-05-24 18:11:43 +0530

    tried CSSTransition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 import './App.css';
 import Modal from './components/Modal';
 import Backdrop from './components/Backdrop';
@@ -19,15 +21,69 @@ class App extends Component {
         });
     };
     render() {
+        let timeout = {
+            enter: 500,
+            exit: 1000,
+        };
         return (
             <div className='App'>
                 <button className='Modal-click' onClick={this.toggleModal}>
                     click me!
                 </button>
-                <Modal
-                    showModal={this.state.modalShow}
-                    closeModal={this.toggleModal}
-                />
+                <CSSTransition
+                    in={this.state.modalShow}
+                    timeout={timeout}
+                    mountOnEnter
+                    unmountOnExit
+                    classNames={{
+                        enterActive: 'openModal',
+                        exitActive: 'closedModal',
+                    }}>
+                    <Modal closeModal={this.toggleModal} />
+                </CSSTransition>
+                {
+                    //without custom classnames
+                }
+                {/* <CSSTransition
+                    in={this.state.modalShow}
+                    timeout={timeout}
+                    mountOnEnter
+                    unmountOnExit
+                    classNames='slide-appear'>
+                        <Modal
+                            closeModal={this.toggleModal}
+                        />
+                </CSSTransition> */}
+                {
+                    // for different timeout for enter and exit
+                }
+                {/* <Transition
+                    in={this.state.modalShow}
+                    timeout={timeout}
+                    mountOnEnter
+                    unmountOnExit>
+                    {(state) => (
+                        <Modal
+                            showModal={state}
+                            closeModal={this.toggleModal}
+                        />
+                    )}
+                </Transition> */}
+                {
+                    // for same timeout for enter and exit
+                }
+                {/* <Transition
+                    in={this.state.modalShow}
+                    timeout={300}
+                    mountOnEnter
+                    unmountOnExit>
+                    {(state) => (
+                        <Modal
+                            showModal={state}
+                            closeModal={this.toggleModal}
+                        />
+                    )}
+                </Transition> */}
                 <Backdrop
                     showModal={this.state.modalShow}
                     closeModal={this.toggleModal}
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,9 +3,12 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-    let classes = ['Modal', props.showModal? 'openModal' : 'closedModal']
+    // using Transition
+    // let classes = ['Modal', props.showModal === 'entering' ? 'openModal' : props.showModal === 'exiting' ? 'closedModal' : null]
+    // using CSSTransition no additional classes required
+
     return (
-        <div className={classes.join(' ')}>
+        <div className='Modal'>
             <p>hi, this is the modal!</p>
             <button className='Dismiss-button' onClick={props.closeModal}>dismiss modal</button>
         </div>
